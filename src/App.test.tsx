39f7708b21a08726ab9c2ed7b86c7e8cb9bbfaa6
Matthew Palmer <38406765+matthewandrewpalmer@users.noreply.github.com>
2021-07-26 13:22:27 +0100

    Last updated time on the homepage

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -6,8 +6,22 @@ import flushPromises from "./tests/utils";
 import {act} from "react-dom/test-utils";
 import {createMemoryHistory} from "history";
 import {Router} from "react-router";
+import {mock_server_request_return_json} from "./tests/utils";
+import MockDate from "mockdate";
+
 
 describe("management information reports homepage", () => {
+    const newMillennium = "2000-01-01";
+    const threeDaysFromTheNewMillennium = "2000-01-03";
+
+    beforeEach(() => {
+        mock_server_request_return_json(200, {"last_updated": new Date(newMillennium).toLocaleDateString()});
+        MockDate.set(new Date(threeDaysFromTheNewMillennium));
+    });
+
+    afterEach(() => {
+        MockDate.reset();
+    });
 
     it("matches snapshot", async () => {
         const history = createMemoryHistory();
@@ -38,6 +52,24 @@ describe("management information reports homepage", () => {
         expect(queryByText(/Interviewer Call History/)).toBeInTheDocument();
         expect(queryByText(/Interviewer Call Pattern/)).toBeInTheDocument();
 
+    });
+
+    it("shows the correct last updated text", async () => {
+        const history = createMemoryHistory();
+        const {queryByText} = render(
+            <Router history={history}>
+                <App/>
+            </Router>
+        );
+
+        await act(async () => {
+            await flushPromises();
+        });
+
+        await waitFor(() => {
+            expect(queryByText(/2 days ago/)).toBeInTheDocument();
+            expect(queryByText(/(01\/01\/2000 00:00:00)/)).toBeInTheDocument();
+        });
     });
 
     afterAll(() => {
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
-import React, {ReactElement} from "react";
+import React, {ReactElement, useEffect, useState} from "react";
 import {DefaultErrorBoundary} from "./components/ErrorHandling/DefaultErrorBoundary";
 import {Link, Route, Switch} from "react-router-dom";
 import {BetaBanner, Footer, Header, ONSPanel} from "blaise-design-system-react-components";
 import InterviewerCallHistory from "./reports/InterviewerCallHistory";
 import InterviewerCallPattern from "./reports/InterviewerCallPattern";
+import {getInterviewerCallHistoryStatus} from "./utilities/http";
+import dateFormatter from "dayjs";
+import TimeAgo from "react-timeago";
 
 const divStyle = {
     minHeight: "calc(67vh)"
 };
 
 function App(): ReactElement {
+    const [reportStatus, setReportStatus] = useState<Date | "">("");
+
+    useEffect(() => {
+        getInterviewerCallHistoryStatus().then(([success, last_updated]) => {
+            console.log(last_updated.last_updated);
+            if (!success) {
+                return;
+            }
+            setReportStatus(new Date(last_updated.last_updated));
+        });
+    }, []);
+
+
     return (
         <>
             <BetaBanner/>
@@ -32,18 +48,20 @@ function App(): ReactElement {
                                     </ONSPanel>
                                 }
                                 <ONSPanel>
+                                    <p aria-live="polite">
+                                        Data in these reports was last updated: <b>
+                                        {<TimeAgo live={false} date={reportStatus}/>}
+                                        {(reportStatus ? "" + dateFormatter(reportStatus).tz("Europe/London").format(" (DD/MM/YYYY HH:mm:ss)"): "Loading")}</b>.
+                                    </p>
                                     <p>
-                                        Data in these reports could be up to 24 hours out of date.
-                                        <br/>
-                                        <br/>
                                         These reports only go back to the last 12 months.
                                     </p>
                                 </ONSPanel>
                                 <br/>
                                 <div className="grid grid--column@xxs@s u-mt-m">
                                     <div className="grid__col col-6@m">
-                                        <div className="card" aria-labelledBy="interviewer-call-history"
-                                             aria-describedBy="interviewer-call-history-text">
+                                        <div className="card" aria-labelledby="interviewer-call-history"
+                                             aria-describedby="interviewer-call-history-text">
                                             <h2 className="u-fs-m" id="interviewer-call-history">
                                                 <Link to="/interviewer-call-history">
                                                     Interviewer Call History
@@ -54,8 +72,8 @@ function App(): ReactElement {
                                         </div>
                                     </div>
                                     <div className="grid__col col-6@m">
-                                        <div className="card" aria-labelledBy="interviewer-call-pattern"
-                                             aria-describedBy="interviewer-call-pattern-text">
+                                        <div className="card" aria-labelledby="interviewer-call-pattern"
+                                             aria-describedby="interviewer-call-pattern-text">
                                             <h2 className="u-fs-m" id="interviewer-call-pattern">
                                                 <Link to="/interviewer-call-pattern">
                                                     Interviewer Call Pattern
@@ -69,8 +87,8 @@ function App(): ReactElement {
                                 <div className="grid grid--column@xxs@s  u-mt-m">
                                     {/* TODO: Removed as the report does not exist yet, to add back in */}
                                     {/*<div className="grid__col col-6@m">*/}
-                                    {/*    <div className="card" aria-labelledBy="appointment-resource-planning"*/}
-                                    {/*         aria-describedBy="appointment-resource-planning-text">*/}
+                                    {/*    <div className="card" aria-labelledby="appointment-resource-planning"*/}
+                                    {/*         aria-describedby="appointment-resource-planning-text">*/}
                                     {/*        <h2 className="u-fs-m" id="appointment-resource-planning">*/}
                                     {/*            <Link to="/appointment-resource-planning">*/}
                                     {/*                Daily Appointment Resource Planning*/}
