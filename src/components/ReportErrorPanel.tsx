970f7de3c7aea31335d86ecb03fe1ab956b2405c
Matthew Palmer <38406765+matthewandrewpalmer@users.noreply.github.com>
2021-08-20 16:26:13 +0100

    Error panel

--- src/components/ReportErrorPanel.tsx
+++ src/components/ReportErrorPanel.tsx
@@ -1,5 +1,6 @@
 import React, {ReactElement, useEffect} from "react";
 import {ONSPanel} from "blaise-design-system-react-components";
+import ErnieFailed from "./ErnieFailed.jpg";
 
 interface Props {
     error: boolean;
@@ -17,6 +18,7 @@ const ReportErrorPanel = ({error}: Props): ReactElement => {
             <div role="alert" ref={input => errorFocus = input} tabIndex={-1}>
                 <ONSPanel status="error">
                     <h2>Failed to run the report</h2>
+                    <img src={ErnieFailed} alt="Ernie from Sesame Street looking at a computer which displays the text NO" width={350} className="u-mb-s"/>
                     <p>Try again later.</p>
                     <p>If you are still experiencing problems <a href="https://ons.service-now.com/">report this
                         issue</a> to Service Desk</p>
