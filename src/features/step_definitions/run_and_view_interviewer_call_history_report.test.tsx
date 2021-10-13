85a7152a206a7e600bbbfa631f52114b0955b2d7
elthorne <46898565+elthorne@users.noreply.github.com>
2021-10-13 13:58:29 +0100

    Commented out code purely for the purpose of deploying to my sandbox and seeing what happens

--- src/features/step_definitions/run_and_view_interviewer_call_history_report.test.tsx
+++ src/features/step_definitions/run_and_view_interviewer_call_history_report.test.tsx
@@ -1,17 +1,17 @@
-import {defineFeature, loadFeature} from "jest-cucumber";
-import {createMemoryHistory} from "history";
-import {cleanup, render, screen, waitFor} from "@testing-library/react";
-import {Router} from "react-router-dom";
+import { defineFeature, loadFeature } from "jest-cucumber";
+import { createMemoryHistory } from "history";
+import { cleanup, render, screen, waitFor } from "@testing-library/react";
+import { Router } from "react-router-dom";
 import App from "../../App";
 import React from "react";
-import {fireEvent} from "@testing-library/dom";
-import {act} from "react-dom/test-utils";
-import flushPromises, {mock_fetch_requests} from "../../tests/utilities";
-import {InterviewerCallHistoryReportData} from "../../interfaces";
+import { fireEvent } from "@testing-library/dom";
+import { act } from "react-dom/test-utils";
+import flushPromises, { mock_fetch_requests } from "../../tests/utilities";
+import { InterviewerCallHistoryReportData } from "../../interfaces";
 
 const feature = loadFeature(
     "./src/features/run_and_view_interviewer_call_history_report.feature",
-    {tagFilter: "not @server and not @integration"}
+    { tagFilter: "not @server and not @integration" }
 );
 
 const reportDataReturned: InterviewerCallHistoryReportData[] = [
@@ -21,7 +21,7 @@ const reportDataReturned: InterviewerCallHistoryReportData[] = [
         serial_number: "1337",
         call_start_time: "Sat, 01 May 2021 10:00:00 GMT",
         dial_secs: "61",
-        number_of_interviews: "42",
+        // number_of_interviews: "42",
         call_result: "Busy",
     }];
 
@@ -35,7 +35,7 @@ const mock_server_responses = (url: string) => {
     } else if (url.includes("/api/reports/call-history-status")) {
         return Promise.resolve({
             status: 200,
-            json: () => Promise.resolve({"last_updated": "Fri, 28 May 2021 10:00:00 GMT"}),
+            json: () => Promise.resolve({ "last_updated": "Fri, 28 May 2021 10:00:00 GMT" }),
         });
     }
 };
@@ -52,12 +52,12 @@ defineFeature(feature, test => {
         mock_fetch_requests(mock_server_responses);
     });
 
-    test("Run and view interviewer call history report", ({given, when, then}) => {
+    test("Run and view interviewer call history report", ({ given, when, then }) => {
         given("An interviewer ID and time period (start date and end date) has been specified", async () => {
             const history = createMemoryHistory();
             render(
                 <Router history={history}>
-                    <App/>
+                    <App />
                 </Router>
             );
 
@@ -94,7 +94,7 @@ defineFeature(feature, test => {
                 expect(screen.getByText(/1337/)).toBeDefined();
                 expect(screen.getByText("01/05/2021 11:00:00")).toBeDefined();
                 expect(screen.getByText(/01:01/)).toBeDefined();
-                expect(screen.getByText(/42/)).toBeDefined();
+                // expect(screen.getByText(/42/)).toBeDefined();
                 expect(screen.getByText(/Busy/)).toBeDefined();
 
             });
--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -3,7 +3,7 @@ interface InterviewerCallHistoryReportData {
     serial_number: string
     call_start_time: string
     dial_secs: string
-    number_of_interviews: string
+    // number_of_interviews: string
     call_result: string
 }
 
