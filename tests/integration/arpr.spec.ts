ffd345dbdedc103850505b10326712090e44b716
elthorne <46898565+elthorne@users.noreply.github.com>
2021-12-31 09:35:40 +0000

    refactor: ARPR Playwright tests
    Added function to try and connect to the blaise api client before attempting anything else.
    
    Moved all functions to below the tests for readability.
    
    Reviewed-by: El
    Refs: BLAIS5-2921 and BLAIS5-2964

--- tests/integration/arpr.spec.ts
+++ tests/integration/arpr.spec.ts
@@ -9,57 +9,54 @@ const REPORTS_URL = process.env.REPORTS_URL;
 const REST_API_URL = process.env.REST_API_URL || "http://localhost:8000";
 const INSTRUMENT_NAME = process.env.TEST_INSTRUMENT;
 
-async function installInstrument(blaiseApiClient: BlaiseApiClient, serverpark: string) {
-  try {
-    await blaiseApiClient.installInstrument(serverpark, {instrumentFile: `${INSTRUMENT_NAME}.bpkg`});
-    for (let attempts = 0; attempts <= 12; attempts++) {
-      const instrumentDetails = await blaiseApiClient.getInstrument(serverpark, INSTRUMENT_NAME);
-      if (instrumentDetails.status == "Active") {
-        break;
-      } else {
-        console.log(`Instrument ${INSTRUMENT_NAME} is not active, waiting to add cases`);
-        await new Promise(f => setTimeout(f, 10000));
-      }
-    }
-    for (let caseID = 1; caseID <= 10; caseID++) {
-      const caseFields = {
-        "qdatabag.telno": "07000 000 000",
-        "qdatabag.telno2": "07000 000 000",
-        "qdatabag.samptitle": "title",
-        "qdatabag.sampfname": "fname",
-        "qdatabag.sampsname": "sname",
-        "qdatabag.name": "name"
-      };
-      await blaiseApiClient.addCase(serverpark, INSTRUMENT_NAME, caseID.toString(), caseFields);
-    }
-  } catch (error) {
-    if (error.code==="ECONNREFUSED"){
-      console.error("Failed to connect to the rest-api.  Please ensure iap tunnel to the rest-api is connected");
-      throw(error);
-    }
-    console.error(`Failed to install instrument: ${error}`);
-    throw(error);
-  }
-}
+test.describe("Without data", () => {
+  test("I can get to, and run an ARPR for a day with no data", async ({ page }) => {
+    await page.goto(`${REPORTS_URL}/`);
+    await page.click("#appointment-resource-planning");
+    await expect(page.locator("h1")).toHaveText("Run appointment resource planning report");
+    await expect(page.locator(".panel--info >> nth=0")).toContainText("Run a Daybatch first to obtain the most accurate results.");
+    await page.locator("#date").type("30-06-1990");
+    await page.click("button[type=submit]");
+    await expect(page.locator(".panel--info >> nth=1")).toHaveText("No data found for parameters given.");
+  });
+});
 
-async function addSurveyDays(blaiseApiClient: BlaiseApiClient, serverpark: string, today: Date, tomorrow: Date) {
-  try {
-    await blaiseApiClient.addSurveyDays(serverpark, INSTRUMENT_NAME, [today.toISOString(), tomorrow.toISOString()]);
-  } catch (error) {
-    console.error(`Failed to add survey days: ${error}`);
-    throw(error);
-  }
+test.describe("With data", () => {
+  test.beforeEach(async ({ page }, testInfo) => {
+    testInfo.setTimeout(120000);
+    console.log(`Running ${testInfo.title}`);
+    await setupInstrument();
+    // CATI seems to be a bit slow on the uptake sometimes...
+    await new Promise(f => setTimeout(f, 10000));
+    await setupAppointment(page);
+  });
 
-}
+  test.afterEach(async ({page}) => {
+    const serverpark = "gusty";
+    const blaiseApiClient = new BlaiseApiClient(REST_API_URL);
+    await clearCATIData(page);
+    blaiseApiClient.deleteInstrument(serverpark, `${INSTRUMENT_NAME}`);
+  });
 
-async function addDaybatch(blaiseApiClient: BlaiseApiClient, serverpark: string, today: Date) {
-  try {
-    await blaiseApiClient.addDaybatch(serverpark, INSTRUMENT_NAME, {dayBatchDate: today.toISOString(), checkForTreatedCases: false});
-  } catch (error) {
-    console.error(`Failed to add daybatch: ${error}`);
-    throw(error);
-  }
-}
+  test("I can get to, and run an ARPR for a day with data", async ({ page }) => {
+    await page.goto(`${REPORTS_URL}/`);
+    await new Promise(f => setTimeout(f, 10000));
+    await page.click("#appointment-resource-planning");
+    await expect(page.locator("h1")).toHaveText("Run appointment resource planning report");
+    await expect(page.locator(".panel--info >> nth=0")).toContainText("Run a Daybatch first to obtain the most accurate results.");
+    await page.locator("#date").type(`${reportTomorrow()}`);
+    await page.click("button[type=submit]");
+
+    // Summary items
+    await expect(page.locator(".summary__item-title")).toHaveText("English");
+    await expect(page.locator(".summary__values")).toHaveText("1");
+
+    // Report items
+    await expect(page.locator(".table__row:has-text('DST2111Z') >> nth=0 >> td >> nth=1")).toHaveText("10:00");
+    await expect(page.locator(".table__row:has-text('DST2111Z') >> nth=0 >> td >> nth=2")).toHaveText("English");
+    await expect(page.locator(".table__row:has-text('DST2111Z') >> nth=0 >> td >> nth=3")).toHaveText("1");
+  });
+});
 
 async function setupInstrument() {
   const blaiseApiClient = new BlaiseApiClient(REST_API_URL);
@@ -68,6 +65,7 @@ async function setupInstrument() {
   const tomorrow = new Date();
   tomorrow.setDate(today.getDate() + 1);
 
+  await connectToRestApi(blaiseApiClient);
   await installInstrument(blaiseApiClient, serverpark);
   await addSurveyDays(blaiseApiClient, serverpark, today, tomorrow);
   await addDaybatch(blaiseApiClient, serverpark, today);
@@ -140,51 +138,63 @@ function reportTomorrow(): string {
   return moment(tomorrow).format("DD/MM/YYYY");
 }
 
-test.describe("Without data", () => {
-  test("I can get to, and run an ARPR for a day with no data", async ({ page }) => {
-    await page.goto(`${REPORTS_URL}/`);
-    await page.click("#appointment-resource-planning");
-    await expect(page.locator("h1")).toHaveText("Run appointment resource planning report");
-    await expect(page.locator(".panel--info >> nth=0")).toContainText("Run a Daybatch first to obtain the most accurate results.");
-    await page.locator("#date").type("30-06-1990");
-    await page.click("button[type=submit]");
-    await expect(page.locator(".panel--info >> nth=1")).toHaveText("No data found for parameters given.");
-  });
-});
+async function connectToRestApi(blaiseApiClient: BlaiseApiClient) {
+  try {
+    await blaiseApiClient.getDiagnostics();
+  } catch (error) {
+    if (error.code==="ECONNREFUSED") {
+      console.error("Failed to connect to the rest-api.  Please ensure iap tunnel to the rest-api is connected");
+      throw(error);
+    }
+    console.error(`Failed to connect to the rest-api: ${error}`);
+    throw(error);
+  }
+}
 
-test.describe("With data", () => {
-  test.beforeEach(async ({ page }, testInfo) => {
-    testInfo.setTimeout(120000);
-    console.log(`Running ${testInfo.title}`);
-    await setupInstrument();
-    // CATI seems to be a bit slow on the uptake sometimes...
-    await new Promise(f => setTimeout(f, 10000));
-    await setupAppointment(page);
-  });
+async function installInstrument(blaiseApiClient: BlaiseApiClient, serverpark: string) {
+  try {
+    await blaiseApiClient.installInstrument(serverpark, {instrumentFile: `${INSTRUMENT_NAME}.bpkg`});
+    for (let attempts = 0; attempts <= 12; attempts++) {
+      const instrumentDetails = await blaiseApiClient.getInstrument(serverpark, `${INSTRUMENT_NAME}`);
+      if (instrumentDetails.status == "Active") {
+        break;
+      } else {
+        console.log(`Instrument ${INSTRUMENT_NAME} is not active, waiting to add cases`);
+        await new Promise(f => setTimeout(f, 10000));
+      }
+    }
+    for (let caseID = 1; caseID <= 10; caseID++) {
+      const caseFields = {
+        "qdatabag.telno": "07000 000 000",
+        "qdatabag.telno2": "07000 000 000",
+        "qdatabag.samptitle": "title",
+        "qdatabag.sampfname": "fname",
+        "qdatabag.sampsname": "sname",
+        "qdatabag.name": "name"
+      };
+      await blaiseApiClient.addCase(serverpark, `${INSTRUMENT_NAME}`, caseID.toString(), caseFields);
+    }
+  } catch (error) {
+    console.error(`Failed to install instrument: ${error}`);
+    throw(error);
+  }
+}
 
-  test.afterEach(async ({page}) => {
-    const serverpark = "gusty";
-    const blaiseApiClient = new BlaiseApiClient(REST_API_URL);
-    await clearCATIData(page);
-    blaiseApiClient.deleteInstrument(serverpark, `${INSTRUMENT_NAME}`);
-  });
+async function addSurveyDays(blaiseApiClient: BlaiseApiClient, serverpark: string, today: Date, tomorrow: Date) {
+  try {
+    await blaiseApiClient.addSurveyDays(serverpark, `${INSTRUMENT_NAME}`, [today.toISOString(), tomorrow.toISOString()]);
+  } catch (error) {
+    console.error(`Failed to add survey days: ${error}`);
+    throw(error);
+  }
 
-  test("I can get to, and run an ARPR for a day with data", async ({ page }) => {
-    await page.goto(`${REPORTS_URL}/`);
-    await new Promise(f => setTimeout(f, 10000));
-    await page.click("#appointment-resource-planning");
-    await expect(page.locator("h1")).toHaveText("Run appointment resource planning report");
-    await expect(page.locator(".panel--info >> nth=0")).toContainText("Run a Daybatch first to obtain the most accurate results.");
-    await page.locator("#date").type(`${reportTomorrow()}`);
-    await page.click("button[type=submit]");
+}
 
-    // Summary items
-    await expect(page.locator(".summary__item-title")).toHaveText("English");
-    await expect(page.locator(".summary__values")).toHaveText("1");
-
-    // Report items
-    await expect(page.locator(".table__row:has-text('DST2111Z') >> nth=0 >> td >> nth=1")).toHaveText("10:00");
-    await expect(page.locator(".table__row:has-text('DST2111Z') >> nth=0 >> td >> nth=2")).toHaveText("English");
-    await expect(page.locator(".table__row:has-text('DST2111Z') >> nth=0 >> td >> nth=3")).toHaveText("1");
-  });
-});
+async function addDaybatch(blaiseApiClient: BlaiseApiClient, serverpark: string, today: Date) {
+  try {
+    await blaiseApiClient.addDaybatch(serverpark, `${INSTRUMENT_NAME}`, {dayBatchDate: today.toISOString(), checkForTreatedCases: false});
+  } catch (error) {
+    console.error(`Failed to add daybatch: ${error}`);
+    throw(error);
+  }
+}
