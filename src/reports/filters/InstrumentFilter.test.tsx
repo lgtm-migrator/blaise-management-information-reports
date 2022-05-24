bbefcda8e4e7670c81c197c297a5b8417e6fd259
Tom Oram <tom.oram@armakuni.com>
2022-05-24 12:42:01 +0100

    test: Tidy up test
    Refs: BLAIS5-3118

--- src/reports/filters/InstrumentFilter.test.tsx
+++ src/reports/filters/InstrumentFilter.test.tsx
@@ -8,11 +8,11 @@ import {cleanup, render, waitFor} from "@testing-library/react";
 import {Router} from "react-router";
 import {act} from "react-dom/test-utils";
 import {screen} from "@testing-library/dom";
-import React, {useState} from "react";
+import React from "react";
 import InstrumentFilter from "./InstrumentFilter";
 import MockAdapter from "axios-mock-adapter";
 import axios from "axios";
-import flushPromises from "../../tests/utilities";
+import {History} from "history";
 
 const mockAdapter = new MockAdapter(axios);
 
@@ -21,17 +21,21 @@ const instrumentDataReturned: string[] = [
 ];
 
 describe("the interviewer details page renders correctly", () => {
-    it("matches snapshot", async () => {
-        const history = createMemoryHistory();
+    let history: History;
 
+    beforeEach(() => {
+        history = createMemoryHistory();
+    });
+
+    it("matches snapshot", async () => {
         mockAdapter.onPost("/api/instruments").reply(200, instrumentDataReturned);
 
         const wrapper = render(
             <Router history={history}>
-                <InstrumentFilter interviewer={"James"}
+                <InstrumentFilter interviewer="James"
                                   startDate={new Date("2022-01-01")}
                                   endDate={new Date("2022-01-05")}
-                                  surveyTla={"LMS"}
+                                  surveyTla="LMS"
                                   instruments={["LMS2101_AA1"]} setInstruments={() => {return;}}
                                   submitFunction={() => {return;}}
                                   navigateBack={() => {return;}}/>
@@ -44,15 +48,13 @@ describe("the interviewer details page renders correctly", () => {
     });
 
     it("renders correctly", async () => {
-        const history = createMemoryHistory();
-
         await act(async () => {
             render(
                 <Router history={history}>
-                    <InstrumentFilter interviewer={"James"}
+                    <InstrumentFilter interviewer="James"
                                       startDate={new Date("2021-01-01")}
                                       endDate={new Date("2021-01-05")}
-                                      surveyTla={"LMS"}
+                                      surveyTla="LMS"
                                       instruments={["LMS2101_AA1"]} setInstruments={() => {return;}}
                                       submitFunction={() => {return;}}
                                       navigateBack={() => {return;}}/>
