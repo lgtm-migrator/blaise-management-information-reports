668d1ab6345faea8260e232cbf990244b9febe12
elthorne <46898565+elthorne@users.noreply.github.com>
2022-07-21 17:54:59 +0100

    Refactor: Remove TODOs.
    Reviewed-by: El
    Refs: BLAIS5-3233

--- src/components/SurveyInterviewerStartDateEndDateForm.tsx
+++ src/components/SurveyInterviewerStartDateEndDateForm.tsx
@@ -49,14 +49,12 @@ const SurveyInterviewerStartDateEndDateForm = ({ onSubmitFunction }: Props): Rea
         {
             name: "Start date",
             type: "date",
-            // TODO: Changing these formats breaks tests
             initial_value: dateFormatter(new Date()).format("YYYY-MM-DD"),
             validate: validateDate
         },
         {
             name: "End date",
             type: "date",
-            // TODO: Changing these formats breaks tests
             initial_value: dateFormatter(new Date()).format("YYYY-MM-DD"),
             validate: validateDate
         }
