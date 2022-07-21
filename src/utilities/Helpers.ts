25d37becc035b4f89ae3f30089c7916e89ff3781
elthorne <46898565+elthorne@users.noreply.github.com>
2022-07-21 15:19:01 +0100

    Refactor: Exctracterton 2000
    Reviewed-by: El
    Refs: BLAIS5-3233

--- src/utilities/Helpers.ts
+++ src/utilities/Helpers.ts
@@ -2,9 +2,11 @@ import dayjs from "dayjs";
 import dateFormatter from "dayjs";
 import utc from "dayjs/plugin/utc";
 import timezone from "dayjs/plugin/timezone";
+import customParseFormat from "dayjs/plugin/customParseFormat";
 
 dateFormatter.extend(utc);
 dateFormatter.extend(timezone);
+dateFormatter.extend(customParseFormat);
 
 export default function subtractYears(numOfYears: number, date: Date = new Date()): Date {
     return dayjs(date).subtract(numOfYears, "year").toDate();
