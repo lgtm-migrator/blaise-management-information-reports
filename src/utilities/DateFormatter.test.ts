d98a0eeeff7f0dde098f5fdb6fb70a3fbd4a92b4
elthorne <46898565+elthorne@users.noreply.github.com>
2022-07-22 12:01:54 +0100

    Refactor: Tests finally added :see_no_evil:
    I feel better now.
    
    Reviewed-by: El
    Refs: BLAIS5-3233

--- src/utilities/DateFormatter.test.ts
+++ src/utilities/DateFormatter.test.ts
@@ -1,6 +1,49 @@
-import subtractYears from "./DateFormatter";
+import subtractYears, { dateFormat, formatDate, formatDateAndTime } from "./DateFormatter";
 
 test("test date returned is 1 year less than date given", () => {
     const date = new Date("1990-06-30");
     expect(String(subtractYears(1, date)) == "1989-06-30");
+});
+
+describe("dateFormat", () => {
+    it("returns a human-readable string format for Brits", async () => {});
+    expect(dateFormat()).toBe("DD/MM/YYYY");
+});
+
+describe("formatDate", () => {
+    afterEach(() => {});
+
+    it("returns a date string when given a date object", async () => {
+        const dateObject = new Date("2022-12-31");
+        expect(formatDate(dateObject)).toBe("31/12/2022");
+    });
+
+    it("returns a date string when given a date string", async () => {
+        const dateString = "2022-12-31";
+        expect(formatDate(dateString)).toBe("31/12/2022");
+    });
+
+    it("returns 'Invalid Date' when given an invalid date string", async () => {
+        const fooString = "foo";
+        expect(formatDate(fooString)).toBe("Invalid Date");
+    });
+});
+
+describe("formatDateAndTime", () => {
+    afterEach(() => {});
+
+    it("returns a date and time string when given a date object", async () => {
+        const dateObject = new Date("2022-12-31 15:45:59.000");
+        expect(formatDateAndTime(dateObject)).toBe("31/12/2022 15:45:59");
+    });
+
+    it("returns a date and time string when given a date string", async () => {
+        const dateString = "2022-12-31 15:45:59.000";
+        expect(formatDateAndTime(dateString)).toBe("31/12/2022 15:45:59");
+    });
+
+    it("returns 'Invalid Date' when given an invalid date string", async () => {
+        const fooString = "foo 15:45:59.000";
+        expect(formatDateAndTime(fooString)).toBe("Invalid Date");
+    });
 });
