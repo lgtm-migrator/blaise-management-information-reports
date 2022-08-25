d4c33f2f3a2e3209f01f011b3c62fcb77d596e71
James Anthony Williams <81305008+JAWilliamsONS@users.noreply.github.com>
2022-08-25 10:15:06 +0100

    wip: extract the report table into a separate component
    Refs: BLAIS5-3214

--- src/reports/InterviewerCallHistory/RenderInterviewerCallHistoryReport.tsx
+++ src/reports/InterviewerCallHistory/RenderInterviewerCallHistoryReport.tsx
@@ -104,57 +104,57 @@ function RenderInterviewerCallHistoryReport(props: RenderInterviewerCallHistoryR
 
                 <ErrorBoundary errorMessageText={"Failed to load"}>
                     {
-                        reportData && reportData.length > 0
-                            ?
-                            <table id="report-table" className="table u-mt-s">
-                                <thead className="table__head u-mt-m">
-                                    <tr className="table__row">
-                                        <th scope="col" className="table__header ">
-                                            <span>Questionnaire</span>
-                                        </th>
-                                        <th scope="col" className="table__header ">
-                                            <span>Serial Number</span>
-                                        </th>
-                                        <th scope="col" className="table__header ">
-                                            <span>Call Start Time</span>
-                                        </th>
-                                        <th scope="col" className="table__header ">
-                                            <span>Call Length</span>
-                                        </th>
-                                        <th scope="col" className="table__header ">
-                                            <span>Call Result</span>
-                                        </th>
-                                    </tr>
-                                </thead>
-                                <tbody className="table__body">
-                                    {
-                                        reportData.map((callHistory: InterviewerCallHistoryReport) => {
-                                            return (
-                                                <tr className="table__row" key={callHistory.call_start_time}
-                                                    data-testid={"report-table-row"}>
-                                                    <td className="table__cell ">
-                                                        {callHistory.questionnaire_name}
-                                                    </td>
-                                                    <td className="table__cell ">
-                                                        {callHistory.serial_number}
-                                                    </td>
-                                                    <td className="table__cell ">
-                                                        {formatDateAndTime(callHistory.call_start_time)}
-                                                    </td>
-                                                    <td className="table__cell ">
-                                                        {convertSecondsToMinutesAndSeconds(callHistory.dial_secs)}
-                                                    </td>
-                                                    <td className="table__cell ">
-                                                        {(callHistory.call_result === null ? "Unknown" : callHistory.call_result)}
-                                                    </td>
-                                                </tr>
-                                            );
-                                        })
-                                    }
-                                </tbody>
-                            </table>
-                            :
-                            <ONSPanel hidden={messageNoData === "" && true}>{messageNoData}</ONSPanel>
+                        // reportData && reportData.length > 0
+                        //     ?
+                        //     <table id="report-table" className="table u-mt-s">
+                        //         <thead className="table__head u-mt-m">
+                        //             <tr className="table__row">
+                        //                 <th scope="col" className="table__header ">
+                        //                     <span>Questionnaire</span>
+                        //                 </th>
+                        //                 <th scope="col" className="table__header ">
+                        //                     <span>Serial Number</span>
+                        //                 </th>
+                        //                 <th scope="col" className="table__header ">
+                        //                     <span>Call Start Time</span>
+                        //                 </th>
+                        //                 <th scope="col" className="table__header ">
+                        //                     <span>Call Length</span>
+                        //                 </th>
+                        //                 <th scope="col" className="table__header ">
+                        //                     <span>Call Result</span>
+                        //                 </th>
+                        //             </tr>
+                        //         </thead>
+                        //         <tbody className="table__body">
+                        //             {
+                        //                 reportData.map((callHistory: InterviewerCallHistoryReport) => {
+                        //                     return (
+                        //                         <tr className="table__row" key={callHistory.call_start_time}
+                        //                             data-testid={"report-table-row"}>
+                        //                             <td className="table__cell ">
+                        //                                 {callHistory.questionnaire_name}
+                        //                             </td>
+                        //                             <td className="table__cell ">
+                        //                                 {callHistory.serial_number}
+                        //                             </td>
+                        //                             <td className="table__cell ">
+                        //                                 {formatDateAndTime(callHistory.call_start_time)}
+                        //                             </td>
+                        //                             <td className="table__cell ">
+                        //                                 {convertSecondsToMinutesAndSeconds(callHistory.dial_secs)}
+                        //                             </td>
+                        //                             <td className="table__cell ">
+                        //                                 {(callHistory.call_result === null ? "Unknown" : callHistory.call_result)}
+                        //                             </td>
+                        //                         </tr>
+                        //                     );
+                        //                 })
+                        //             }
+                        //         </tbody>
+                        //     </table>
+                        //     :
+                        //     <ONSPanel hidden={messageNoData === "" && true}>{messageNoData}</ONSPanel>
                     }
                     <br/>
                 </ErrorBoundary>
