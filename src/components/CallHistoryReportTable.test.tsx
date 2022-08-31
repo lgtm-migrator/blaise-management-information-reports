7f7a06d9109d0338b2e54fc4853cd7035bd5da6b
James Anthony Williams <81305008+JAWilliamsONS@users.noreply.github.com>
2022-08-31 09:43:36 +0100

    test: add additional unit tests
    Refs: BLAIS5-3214

--- src/components/CallHistoryReportTable.test.tsx
+++ src/components/CallHistoryReportTable.test.tsx
@@ -1,32 +1,20 @@
 import "@testing-library/jest-dom";
-import { createMemoryHistory } from "history";
-import { render } from "@testing-library/react";
-import { Router } from "react-router";
+import {createMemoryHistory} from "history";
+import {render} from "@testing-library/react";
+import {Router} from "react-router";
 import CallHistoryReportTable from "./CallHistoryReportTable";
-import { act } from "react-dom/test-utils";
+import {act} from "react-dom/test-utils";
 import React from "react";
-import { screen } from "@testing-library/dom";
+import {screen} from "@testing-library/dom";
 import flushPromises from "../tests/utilities";
-import { InterviewerCallHistoryReport } from "../interfaces";
+import {InterviewerCallHistoryReport} from "../interfaces";
 
 const reportData: InterviewerCallHistoryReport = {
-    appointment_info: "foo",
-    busy_dials: 1,
-    call_number: 1,
-    cohort: "1",
-    update_info: "FOO",
-    wave: "1",
-    questionnaire_id: "LMS",
-    interviewer: "James",
-    outcome_code: 310,
-    dial_number: 1,
-    status: "1",
-    survey: "LMS",
-    call_result: "310",
-    dial_secs: 100,
+    call_result: "Appointment",
+    dial_secs: 90,
     questionnaire_name: "LMS2101_AA1",
     serial_number: "900001",
-    call_start_time: "09:00 31/01/2022"
+    call_start_time: "2022-01-31T09:00-0000"
 };
 const messageNoData = "No data found for parameters given.";
 
@@ -55,12 +43,68 @@ describe("CallHistoryReportTable", () => {
         });
         expect(screen.queryByText("Questionnaire")).toBeVisible();
     });
-});
 
-// it(displays a heading of 'Serial number')
-// it(displays a heading of 'Call start time')
-// it(displays a heading of 'Call length')
-// it(displays a heading of 'Call result')
-// it(displays a bold line beneath the heading)
-// it(displays the relevant information for each heading)
-// it(display a line beneath each row)
+    it("displays a heading of 'Serial number'", async () => {
+        const history = createMemoryHistory();
+        await act(async () => {
+            render(
+                <Router history={history}>
+                    <CallHistoryReportTable messageNoData={messageNoData} reportData={[reportData]}/>
+                </Router>
+            );
+        });
+        expect(screen.queryByText("Serial Number")).toBeVisible();
+    });
+
+    it("displays a heading of 'Call Start Time'", async () => {
+        const history = createMemoryHistory();
+        await act(async () => {
+            render(
+                <Router history={history}>
+                    <CallHistoryReportTable messageNoData={messageNoData} reportData={[reportData]}/>
+                </Router>
+            );
+        });
+        expect(screen.queryByText("Call Start Time")).toBeVisible();
+    });
+
+    it("displays a heading of 'Call Length'", async () => {
+        const history = createMemoryHistory();
+        await act(async () => {
+            render(
+                <Router history={history}>
+                    <CallHistoryReportTable messageNoData={messageNoData} reportData={[reportData]}/>
+                </Router>
+            );
+        });
+        expect(screen.queryByText("Call Length")).toBeVisible();
+    });
+
+    it("displays a heading of 'Call Result'", async () => {
+        const history = createMemoryHistory();
+        await act(async () => {
+            render(
+                <Router history={history}>
+                    <CallHistoryReportTable messageNoData={messageNoData} reportData={[reportData]}/>
+                </Router>
+            );
+        });
+        expect(screen.queryByText("Call Result")).toBeVisible();
+    });
+
+    it("displays the relevant information for each heading", async () => {
+        const history = createMemoryHistory();
+        await act(async () => {
+            render(
+                <Router history={history}>
+                    <CallHistoryReportTable messageNoData={messageNoData} reportData={[reportData]}/>
+                </Router>
+            );
+        });
+        expect(screen.queryByText("LMS2101_AA1")).toBeVisible();
+        expect(screen.queryByText("900001")).toBeVisible();
+        expect(screen.queryByText("31/01/2022 09:00:00")).toBeVisible();
+        expect(screen.queryByText("01:30")).toBeVisible();
+        expect(screen.queryByText("Appointment")).toBeVisible();
+    });
+});
