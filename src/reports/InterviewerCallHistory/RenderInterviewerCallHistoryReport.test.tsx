2f9c6aa4db6943619ffa177dbad8bc3a643aabe3
Tom Oram <tom.oram@armakuni.com>
2022-10-11 15:40:14 +0100

    test: Remove old attributes
    Refs: BLAIS5-3409

--- src/reports/InterviewerCallHistory/RenderInterviewerCallHistoryReport.test.tsx
+++ src/reports/InterviewerCallHistory/RenderInterviewerCallHistoryReport.test.tsx
@@ -41,10 +41,6 @@ describe("RenderInterviewerCallHistoryReport", () => {
             <Router history={ history }>
                 <RenderInterviewerCallHistoryReport
                     interviewerFilterQuery={interviewerFilterQuery}
-                    interviewer="rich"
-                    startDate={ new Date("September 18, 2022 01:23:00") }
-                    endDate={ new Date("October 17, 2022 04:56:00") }
-                    surveyTla="LMS"
                     questionnaires={ ["LMS1111", "LMS2222"] }
                     navigateBack={ navigateBack }
                     navigateBackTwoSteps={ navigateBackTwoSteps }
